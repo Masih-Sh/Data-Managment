029a9705f1fcea0b0fa6666732fcd91c73202679
unknown <c00235507@itcarlow.ie>
2021-10-20 09:28:42 +0100

    blacked

--- Learning/app.py
+++ Learning/app.py
@@ -1,18 +1,18 @@
-
 from flask import Flask
-
 import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")     #register the / URL with the web server.
+@app.route("/")  # register the / URL with the web server.
 def index():
-    return datetime.datetime.now().ctime()      #the HTTP response
+    return datetime.datetime.now().ctime()  # the HTTP response
+
 
 @app.route("/hello")
 def hello():
     return "Hello from my first webapp"
-    
+
+
 if __name__ == "__main__":
     app.run()
