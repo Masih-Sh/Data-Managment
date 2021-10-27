e10c2720539b4df2795fcf0f8e5104cffb1ded81
unknown <c00235507@itcarlow.ie>
2021-10-27 10:40:44 +0100

    everything works!

--- Learning/Webapp/app.py
+++ Learning/Webapp/app.py
@@ -1,18 +1,39 @@
-
-from flask import Flask
-
+from flask import Flask, request, render_template
 import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")  #resigter the / URL with the Flask web server
+@app.route("/")  # resigter the / URL with the Flask web server
 def index():
-    return datetime.datetime.now().ctime()      #the HTTP responce
+    return datetime.datetime.now().ctime()  # the HTTP responce
+
 
 @app.route("/hello")
 def hello():
     return "Hello I finally understand what is going on here!"
 
-if __name__ =="__main__":
-    app.run() 
+@app.get("/home")
+def home_page():
+    with open("home.html") as f:
+        html = f.read()
+    return html
+
+
+@app.get("/showform")
+def display_form():
+    with open("form.html") as f:
+        html = f.read()
+    return html
+
+@app.post("/processform")
+def process_form():
+    the_name = request.form["thename"]
+    the_dob = request.form["thedob"]
+    with open("suckers.text", "a") as sf:
+        print(f"{the_name}, {the_dob}", file=sf)
+    return f"Hi there , {the_name} , we know you were born on : {the_dob} ."
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
